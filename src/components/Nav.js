694f215dd436ee3eda67b9be244e4ddff544ea87
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-23 10:31:30 -0500

    futher nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,18 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import navStyles from '../css/navStyles.css'
+import navStyles from '../css/navStyles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navigation() {
     return (
         <Container fluid className="header">
-            <Navbar expand="lg" className="">
-                <Col>
+            <Navbar expand="lg">
+                <Col className="col-12 col-lg-6 d-flex justify-content-center">
+                <h1>Matt Chiaro</h1>
                 </Col>
-                <Col className="">
-                    <Navbar.Collapse className="basic-navbar-nav paralellogram">
+                <Col className="col-12 col-lg-6">
+                    <Navbar className="basic-navbar-nav paralellogram">
                         <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#link">About</Nav.Link>
@@ -20,7 +22,7 @@ function Navigation() {
                             <Nav.Link href="#link">Resume</Nav.Link>
                             <Nav.Link href="#link">Contact</Nav.Link>
                         </Nav>
-                    </Navbar.Collapse>
+                    </Navbar>
                 </Col>
             </Navbar>
         </Container>
