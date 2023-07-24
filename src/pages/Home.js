0bfdfb75d86aa9029821e9f8e93d3fc4668fc03d
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-24 17:50:33 -0500

    Landing page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function Home() {
+    return (
+        <Container fluid className="home">
+            <h1>Home</h1>
+        </Container>
+    );
+}
+
+export default Home
