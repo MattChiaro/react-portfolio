3fe0df8f0b520605839fd4f5ee31b33fb848b487
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-24 12:46:04 -0500

    nested nav within header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import Navigation from './Nav';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SlideIn from '../utils/SlideIn';
+import Navigation from './Nav';
+import nameSVG from '../name.svg';
+import '../css/headerStyles.css';
 
 function Header() {
-    <Container>
-    <Navbar expand="lg">
-        <Col></Col>
-        <Navigation />           
-        </Navbar>
+    return (
+        <Container fluid className="header">
+            <Navbar expand="lg">
+                <Col className="col-12 col-lg-4 d-flex justify-content-center">
+                    <img src={nameSVG}/>
+                </Col>
+                <Col>
+                </Col>
+                <Col className="col-12 col-lg-5">
+                    <SlideIn>
+                    <Navigation />
+                </SlideIn></Col>
+
+            </Navbar>
         </Container>
-    }
+    )
+}
 
 export default Header;
