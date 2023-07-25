750eccc8a395def631a30701bd52bcd2033694fd
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-24 20:37:51 -0500

    Typewriter, landing page complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Typewriter from "typewriter-effect";
 
 function Home() {
-    return (
-        
-            <h1>Home</h1>
-        
-    );
+  return (
+    <div className="content row">
+      <Container className="d-flex justify-content-center align-items-center">
+        <div className="text-center typewriter">
+            <Typewriter
+            onInit={(typewriter) => {
+                typewriter
+                .changeDelay(50)
+                .typeString("Hey! I'm Matt.")
+                .pauseFor(1000)
+                .typeString("<br>I'm a Full Stack Web Developer.")
+                .pauseFor(1000)
+                .typeString("<br>Welcome to my Portfolio!")
+                .start();
+            }}
+            />
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
