27eea91c948bdd936925a9a638caee4ceb3223f4
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-11-30 12:33:58 -0600

    Contact form functioning

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,81 @@
 import { Button, Form, Container } from 'react-bootstrap';
+import { useForm, ValidationError, FormspreeProvider, Field, FormspreeForm } from '@formspree/react';
 
 function Contact() {
+    const [state, handleSubmit] = useForm("mbjvvqyy");
+
+    if (state.succeeded) {
+        return (
+            <Container className="d-flex justify-content-center text-center mt-5">
+            <div className="col-8">
+        <p className='display-5'>Thanks! We will talk soon!</p>
+        </div>
+        </Container>
+        )
+    }
+
     return (
-        <Container>
-            <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Name:</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address:</Form.Label>
-                    <Form.Control type="email"/>
-                </Form.Group>
+        <Container className="d-flex justify-content-center align-items-center">
+            <div className="col-8">
+        <form onSubmit={handleSubmit} className="m-5">
+            <div className="mb-3">
+                <label className="form-label">
+                    Name
+                </label>
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    className="form-control"
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                    Email Address
+                </label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    className="form-control"
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+            </div>
 
-                <Form.Group className="mb-3" controlId="formFeedback">
-                    <Form.Label>What can I do for you?</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
 
-                </Form.Group>
-                <Button className="btn" variant="secondary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+            <div className="mb-3">
+                <label htmlFor="message" className="form-label">
+                    What can I do for you?
+                </label>
+                <textarea
+                    id="message"
+                    name="message"
+                    className="form-control"
+                />
+                <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                />
+            </div>
+
+            <button type="submit" className="btn btn-secondary" disabled={state.submitting}>
+                Submit
+            </button>
+        </form>
+        </div>
         </Container>
     );
 }
+
 
 export default Contact;
